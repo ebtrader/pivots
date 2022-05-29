6ba2c592fd137ddf13de1c6a574f2c5b59ba1e1f
ebtrader <jsiddique@gmail.com>
2022-05-29 18:52:05 -0400

    commit

--- plotting.py
+++ plotting.py
@@ -5,31 +5,37 @@ import numpy as np
 
 # https://raposa.trade/blog/higher-highs-lower-lows-and-calculating-price-trends-in-python/
 
+# https://plotly.com/python/marker-style/
+
 ticker = 'NQ=F'
 
-#df = yf.download(tickers=ticker, period='6mo', interval='1d')
-df = yf.download(tickers = ticker, start='2022-01-04', end='2022-05-26')
+df = yf.download(tickers=ticker, period='6mo', interval='1d')
+#df = yf.download(tickers = ticker, start='2013-01-01', end='2014-12-31')
 
 df = df.reset_index()
 
-max_idx = argrelextrema(df['Close'].values, np.greater, order=5)[0]
-min_idx = argrelextrema(df['Close'].values, np.less, order=5)[0]
+Order = 5
+
+max_idx = argrelextrema(df['Close'].values, np.greater, order=Order)[0]
+min_idx = argrelextrema(df['Close'].values, np.less, order=Order)[0]
 
 
 fig1 = go.Figure(data=[go.Candlestick(x=df['Date'],
                                       open=df['Open'],
                                       high=df['High'],
                                       low=df['Low'],
-                                      close=df['Close'])])
-Size = 10
+                                      close=df['Close'], showlegend=False)])
+Size = 15
 Width = 1
 
 fig1.add_trace(
     go.Scatter(
+        name='sell here',
         mode='markers',
         x=df.iloc[max_idx]['Date'],
         y=df.iloc[max_idx]['High'],
         marker=dict(
+            symbol=46,
             color='darkred',
             size=Size,
             line=dict(
@@ -37,16 +43,18 @@ fig1.add_trace(
                 width=Width
             )
         ),
-        showlegend=False
+        showlegend=True
     )
 )
 
 fig1.add_trace(
     go.Scatter(
+        name='buy here',
         mode='markers',
         x=df.iloc[min_idx]['Date'],
         y=df.iloc[min_idx]['Low'],
         marker=dict(
+            symbol=45,
             color='forestgreen',
             size=Size,
             line=dict(
@@ -54,11 +62,15 @@ fig1.add_trace(
                 width=Width
             )
         ),
-        showlegend=False
+        showlegend=True
     )
 )
 
 # fig1.show()
+
+fig1.update_layout(
+    title=ticker, xaxis_rangeslider_visible=False
+)
 
 fig1.write_html( 'output_file_name.html',
                    auto_open=True )
